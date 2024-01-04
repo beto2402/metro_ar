1f3c085694e400ab74e1915da234698f552d880e
beto2402 <adibeto72@gmail.com>
2024-01-04 10:13:43 -0600

    Remove unnecesaries

--- shape_detection.py
+++ shape_detection.py
@@ -13,48 +13,10 @@ cap.set(4, frame_height)
 def empty(_):
     pass
 
-cv2.namedWindow("Parameters")
-cv2.resizeWindow("Parameters", 640, 240)
-cv2.createTrackbar("Threshold1", "Parameters", 48, 255, empty)
-cv2.createTrackbar("Threshold2", "Parameters", 15, 255, empty)
-
 
 cv2.namedWindow("Prediction")
 cv2.resizeWindow("Prediction", 640, 240)
 cv2.createTrackbar("enabled", "Prediction", 1, 1, empty)
-
-
-
-def stack_images(scale, img_array):
-    rows = len(img_array)
-    cols = len(img_array[0])
-    rowsAvailable = isinstance(img_array[0], list)
-    width =img_array[0][0].shape[1]
-    height = img_array[0][0].shape[0]
-    if rowsAvailable:
-        for x in range (0, rows):
-            for y in range(0, cols):
-                if img_array[x][y].shape[:2] == img_array[0][0].shape[:2]:
-                    img_array[x][y] = cv2.resize(img_array[x][y], (0,0), None, scale, scale)
-                else:
-                    img_array[x][y] = cv2.resize(img_array[x][y], (img_array[0][0].shape[1], img_array[0][0].shape[0]), None, scale, scale)
-                if len(img_array[x][y].shape) == 2: img_array[x][y] = cv2.cvtColor(img_array[x][y], cv2.COLOR_GRAY2BGR)
-        image_blank = np.zeros((height, width, 3), np.uint8)
-        hor = [image_blank]*rows
-        hor_con = [image_blank] *rows
-        for x in range (0,rows):
-            hor[x] = np.hstack(img_array[x])
-        ver = np.vstack(hor)
-    else:
-        for x in range(0,rows):
-            if img_array[x].shape[:2] == img_array[0].shape[:2]:
-                img_array[x] = cv2.resize(img_array[x], (0,0), None, scale, scale)
-            else:
-                img_array[x] = cv2.resize(img_array[x], (int(img_array[0].shape[1]), img_array[0].shape[0]), None, scale, scale)
-            if len(img_array[x].shape) ==2: img_array[x] = cv2.cvtColor(img_array[x], cv2.COLOR_GRAY2BGR)
-        hor = np.hstack(img_array)
-        ver = hor
-    return ver
 
 
 start = 0
@@ -127,9 +89,6 @@ while True:
 
     t_1 = 48
     t_2 = 15 # 67?
-
-    threshold_1 = cv2.getTrackbarPos("Threshold1", "Parameters")
-    threshold_2 = cv2.getTrackbarPos("Threshold2", "Parameters")
 
     img_canny = cv2.Canny(img_gray, t_1, t_2)
 
