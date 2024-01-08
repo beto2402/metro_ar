17b3141ec6b790f7a1d2380fa1d6ce7619ffe422
beto2402 <adibeto72@gmail.com>
2024-01-07 22:56:20 -0600

    Miris

--- run_data_augmentation.py
+++
@@ -1,9 +0,0 @@
-from data_augmentation import augment_dataset
-
-
-input_folder = 'datasets/mios/resized'
-output_base_folder = 'datasets/mios/augs'
-
-
-augment_dataset(input_folder, output_base_folder)
-
--- run_predictor.py
+++
@@ -1,5 +0,0 @@
-from metro_predictor import MetroPredictor
-
-metro_predictor = MetroPredictor(grayscale=False, recognition_time=0.5)
-
-metro_predictor.start_process()
