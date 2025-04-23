8950cb3b13f3e21b8dc75f71b1266800eedf6087
beto2402 <adibeto72@gmail.com>
2025-04-22 19:35:56 -0600

    Fix functionality, formatting, and added logs for scrapper

--- scrapper.py
+++ scrapper.py
@@ -2,7 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager # pip install webdriver-manager
+# pip install webdriver-manager
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium import webdriver
@@ -11,83 +12,90 @@ from PIL import Image
 import os
 from preprocess import resize_and_save, save_png_as_jpg
 
-
-la_red_dropdown_xpath = "((//a[@href='https://www.metro.cdmx.gob.mx/la-red'])[1]//following::ul[1]//child::a[contains(@href, 'www')])"
-base_url = "https://www.metro.cdmx.gob.mx"
-
-base_path = "datasets/mios"
-
-if not os.path.exists(base_path): 
-    os.makedirs(base_path)
-
-opts = Options()
-opts.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.5938.152 Safari/537.36")
-
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=opts)
-
-driver.get(base_url)
+import constants
 
 
-lines_links_elements = [ele.get_attribute("href") for ele in driver.find_elements(By.XPATH, la_red_dropdown_xpath)]
+def initialize_folders():
+    for folder in constants.REQUIRED_PATHS:
+        if not os.path.exists(folder):
+            os.makedirs(folder)
 
 
-for i, line_url in enumerate(lines_links_elements):
-    # The first link goes to a map, not to a line
-    if i == 0:
-        continue
-    
-    driver.get(line_url)
+def build_driver():
+    browser_options = Options()
+    browser_options.add_argument(
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 13_2) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.3 Safari/605.1.15"
+    )
 
-    line_id = i
+    return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=browser_options)
 
-    if i == 10:
-        line_id = "a"
-    elif i == 11:
-        line_id = "b"
+
+def get_lines_urls_info():
+    lines_dropdown_xpath = f"(//a[@href='{constants.BASE_URL}/la-red'])[1]"
+    metro_lines_dropdown_elements_xpath = f"({lines_dropdown_xpath}//following::ul[1]//child::a[contains(@href, 'www')])"
+
+    driver.get(constants.BASE_URL)
+    lines_dropdown = driver.find_elements(By.XPATH, lines_dropdown_xpath)[0]
+    lines_dropdown.click()
+
+    target_urls_info = []
+
+    for dropdown_element in driver.find_elements(By.XPATH, metro_lines_dropdown_elements_xpath):
+        target_urls_info.append({
+            "target_url": dropdown_element.get_attribute("href"),
+            "name": dropdown_element.text,
+        })
+
+    return target_urls_info
+
+def handle_line_url_info(url_info):
+    # Format is "Linea ID", so we only keep what's after the space
+    line_id = url_info["name"].split(" ")[1].strip().lower()
+
+    driver.get(url_info["target_url"])
 
     page_images_xpath = f"//img[contains(@src, 'linea{line_id}')]"
 
-    stations_images_elements = driver.find_elements(By.XPATH, page_images_xpath)
+    stations_images_elements = driver.find_elements(
+        By.XPATH, page_images_xpath)
 
-
-    for j, station_element in enumerate(stations_images_elements):
+    print(f"-----PROCESSING LINE '{line_id}'-----")
+    for station_element in stations_images_elements:
         # The width of the icons is not higher than 100
         if int(station_element.get_attribute("width")) > 100:
             continue
 
         url = station_element.get_attribute("src")
         station_name = url.split("/")[-1].split(".")[0]
-        ogs_path = f"{base_path}/ogs"
-        og_img_path = f"{ogs_path}/{station_name}.png"
+
+        print(f"Saving station '{station_name}'")
+
+        og_img_path = f"{constants.ORIGINAL_IMAGES_PATH}/{station_name}.png"
 
         if os.path.exists(og_img_path):
             continue
 
-        
-        if not os.path.exists(ogs_path): 
-           os.makedirs(ogs_path)
-        
-        
-
-        resizeds_path = f"{base_path}/resized"
-        if not os.path.exists(resizeds_path):
-           os.makedirs(resizeds_path)
-
-        
-        jpgs_ogs_path = f"{base_path}/jpgs"
-        if not os.path.exists(jpgs_ogs_path):
-           os.makedirs(jpgs_ogs_path)
-        
-
         urllib.request.urlretrieve(url, og_img_path)
 
-        jpg_og_img_path = f"{base_path}/jpgs/{station_name}.jpg"
+        jpg_og_img_path = f"{constants.BASE_PATH}/jpgs/{station_name}.jpg"
 
-        save_png_as_jpg(og_img_path, new_path)
-        
-        resized_img_path = f"{base_path}/resized/{station_name}.jpg"
+        save_png_as_jpg(og_img_path, jpg_og_img_path)
 
-        new_path = resize_and_save(jpg_og_img_path, resized_img_path)
+        resized_img_path = f"{constants.BASE_PATH}/resized/{station_name}.jpg"
+
+        resize_and_save(jpg_og_img_path, resized_img_path)
 
 
+initialize_folders()
+driver = build_driver()
 
+target_urls_info = get_lines_urls_info()
+
+
+for url_info in target_urls_info:
+
+    # Handle Map differently
+    if "Mapa" in url_info["name"]:
+        continue
+    elif "Línea" in url_info["name"]:
+        handle_line_url_info(url_info)
